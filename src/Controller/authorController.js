7bda82bbf61ea30c3ef024f8c08d1cf8a8c9ca3a
amanprajapat08278 <amanprajapat08278@gmail.com>
2022-11-14 21:37:18 +0530

    p1 phase-1

--- src/Controller/authorController.js
+++ src/Controller/authorController.js
@@ -9,24 +9,33 @@ const createAuthor = async function (req, res) {
         if (!fname || !lname || !title || !email || !password) {
             res.status(400).send("All fields are mandatory")
         }
-          
+
         function validateEmail(email) {
             var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
         }
         let checkEmail = validateEmail(email)
 
-        if(!checkEmail){
+
+        function checkPassword(str) {
+            var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+            return re.test(str);
+        }
+        let checkPass = checkPassword(password)
+
+
+        if (!checkEmail) {
             res.status(400).send("Please enter a valid Email")
-        }else{
+        }else if(!checkPass) {
+            res.status(400).send("Please enter a valid Password")
+        }
+        else {
             let result = await authorModel.create(data);
             res.status(201).send({ status: true, msg: result })
         }
 
-        
-
-    }catch(err){ 
-        res.status(500).send(err.message) 
+    } catch (err) {
+        res.status(500).send(err.message)
     }
 };
 
--- z.js
+++ z.js
@@ -1,2 +1,17 @@
+// function validateEmail(email) {
+//     var re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+//     return re.test(email);
+// }
+// let checkEmail = validateEmail("aman@8278")
+// console.log(checkEmail)
 
 
+
+function checkPassword(str)
+{
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(str);
+}
+
+let checkEmail = checkPassword("Aman@8278")
+console.log(checkEmail)
